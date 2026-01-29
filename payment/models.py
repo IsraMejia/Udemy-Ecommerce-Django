52bdfc053b56e2709ab39561c9a257d8f663272b
Israel Mejia <ihmaisrael@gmail.com>
2026-01-28 23:52:44 -0600

    adjustment in payment models

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 class ShippingAddress(models.Model):
     
@@ -22,3 +23,31 @@ class ShippingAddress(models.Model):
         
     def  __str__(self):
         return 'Shipping Address - ' + str(self.id)
+    
+    
+    
+class Order(models.Model): 
+    
+    full_name = models.CharField(max_length= 300)
+    email = models.EmailField(max_length=255)
+    shippingAddress = models.TextField(max_length=10000)
+    amount_paid = models.DecimalField(max_digits=8 , decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now=True)
+    #ForeingKey
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True , blank=True)
+    
+    def __str__(self):
+        return 'Order - #' + str(self.id)
+    
+    
+    
+class OrderItem(models.Model):
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+    
+    
+    
+    
+    
+    
+    
